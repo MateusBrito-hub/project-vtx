12613b4b449d1213c47a5fa09e2b73acda0d17be
Mateus Brito <mateusvsbrito6@gmail.com>
2026-08-19 12:52:05 -0300

    Feat: Adiciona coluna de status na tabela Plan e ajusta rotas para atualização e suspensão de planos

--- src/router/plam.ts
+++ src/router/plam.ts
@@ -13,8 +13,8 @@ const router = Router()
 router.post('/', registerPlan)
 router.get('/', getPlans)
 router.get('/:id', getPlan)
-router.put('/:id', updatePlanById)
-router.delete('/:id', suspendPlanById)
+router.patch('/:id', updatePlanById)
+router.patch('/:id/suspend', suspendPlanById)
 router.patch('/:id/activate', activatePlanById)
 
 export default router
